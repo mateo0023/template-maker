e286e2e0e3458d63750eee81d72312005e3679b3
Mateo Aberastury <mateo.aberastury@gmail.com>
2022-05-12 09:31:43 -0400

    Initial 1.0.2 version

---
+++ src/app.js
@@ -0,0 +1,337 @@
+const { app, BrowserWindow, Menu, ipcMain, dialog, Notification, shell } = require('electron')
+const prompt = require('electron-prompt');
+const path = require('path')
+const fs = require('fs')
+const sharp = require('sharp');
+const archiver = require('archiver');
+const { homedir } = require('os');
+
+// So that images are freed from the system after using them
+sharp.cache(false)
+
+function emptyCollectionObj() {
+    return { articles: [] }
+}
+
+var mainWindow;
+var working_path = `${path.join(require("os").homedir(), '/Documents')}`
+var working_file;
+
+app.whenReady().then(() => {
+    const createWindwow = () => {
+        mainWindow = new BrowserWindow({
+            title: "Template Maker",
+            width: 900,
+            height: 700,
+            minWidth: 700,
+            minHeight: 700,
+            icon: './assets/SolveIt-Logo.ico',
+            maximizable: true,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false,
+            },
+        })
+
+        // mainWindow.toggleDevTools();
+
+        mainWindow.loadFile('./src/main.html')
+
+        // Kill the app when main Window closed
+        mainWindow.on('closed', function () {
+            app.quit()
+        })
+
+        // Create the menu object and add it to the application
+        const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+        Menu.setApplicationMenu(mainMenu)
+    }
+
+    createWindwow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.setAboutPanelOptions({
+    applicationName: "Template Maker",
+    applicationVersion: app.getVersion(),
+    authors: ["Mateo Aberastury"],
+    iconPath: "./assets/SolveIt Logo.png"
+})
+
+app.setName("Template Maker")
+
+
+ipcMain.on('save-request', (e, data) => {
+    writeToFile(data);
+})
+
+// The response from the file:save request made to the webWindow
+ipcMain.on('file:save-response', (e, data) => {
+    writeToFile(data)
+    notify("Successfully Saved Progress")
+})
+
+// New Title Request
+ipcMain.on('ask-title', () => {
+    prompt({
+        title: "Enter the Article's title",
+        label: 'Title:',
+        type: 'input'
+    })
+        .then((r) => {
+            if (r === null) {
+                mainWindow.webContents.send('title-response', "empty")
+            } else {
+                mainWindow.webContents.send('title-response', r)
+            }
+        })
+})
+
+// Image Selection request
+ipcMain.handle('select-image', async () => {
+    let img_path = dialog.showOpenDialogSync({
+        title: "Select an image",
+        filters: [
+            { name: 'Images', extensions: ['jpg', 'png', 'jpeg', 'webp'] },
+            { name: 'All Files', extensions: ['*'] }
+        ],
+        properties: [
+            'openFile'
+        ],
+        defaultPath: working_path
+    })
+
+    if (img_path === undefined)
+        return { rel: '', abs: '' }
+    else
+        img_path = `${img_path}`
+
+    let relative_path = `${path.relative(working_path, img_path)}`
+    let image_name = path.basename(img_path)
+
+    // Users might select a file that's outside the supposed folder
+    // In that case, file must be moved (or saved sharp) to a sub-folder
+
+    const img = await sharp(img_path)
+    const meta = await img.metadata()
+    let new_img_path = img_path
+
+    const adjust_img_path = () => {
+        if(!fs.existsSync(path.join(working_path, "src"))){
+            fs.mkdirSync(path.join(working_path, "src"), {recursive: true})
+        }
+        new_img_path = path.join(working_path, 'src', image_name)
+    }
+
+    if (meta.format === 'webp') {
+        adjust_img_path()
+        new_img_path = new_img_path.replace('.webp', '.jpeg')
+        await img.resize(1080, 1350, { fit: "outside" }).toFile(new_img_path)
+        fs.rm(img_path, (e) => {if(e) console.log(e)})
+    } else if (meta.width > 1080 && meta.height > 1350) {
+        adjust_img_path()
+        console.log("renaming")
+        // Only add the _s to the file name if it doesn't exist inside ./src
+        if(fs.existsSync(new_img_path)){
+            new_img_path = new_img_path.split('.')
+            new_img_path[new_img_path.length - 2] = new_img_path[new_img_path.length - 2] + '_s'
+            new_img_path = new_img_path.join('.')
+        }
+
+        await img.resize(1080, 1350, { fit: "outside" }).toFile(new_img_path)
+        fs.rm(img_path, (e) => {if(e) console.log(e)})
+    } else if (relative_path.startsWith('..')) {
+        adjust_img_path()
+        fs.renameSync(img_path, new_img_path)
+    }
+
+    console.log(`About to return ${path.relative(working_path, new_img_path)}`)
+    return { rel: `${path.relative(working_path, new_img_path)}`, abs: `${new_img_path}` }
+})
+
+// Get the control key - OS Dependent
+const ctrl = (process.platform == 'darwin') ? 'Command' : 'Ctrl'
+
+const mainMenuTemplate = [
+    ...(process.platform === 'darwin' ? [{ label: app.name }] : []),
+    {
+        label: '&File',
+        submenu: [
+            {
+                label: 'Open',
+                accelerator: `${ctrl}+O`,
+                click() {
+                    openFile()
+                }
+            },
+            {
+                label: 'Save',
+                accelerator: `${ctrl}+S`,
+                click() {
+                    sendDataRequest();
+                }
+            },
+            {
+                label: 'Export To Zip',
+                accelerator: `${ctrl}+E`,
+                click() {
+                    mainWindow.webContents.send('export-request')
+                    ipcMain.once('export-response', (e, data) => {
+                        writeToFile(data)
+                        let zip_path = path.join(working_path, 'SHARE_THIS.zip')
+                        const output = fs.createWriteStream(zip_path);
+                        const archive = archiver('zip');
+
+                        output.on('close', () => {
+                            notify("Succesfully Exported", `Saved to: ${zip_path}`)
+                            shell.showItemInFolder(zip_path)
+                        });
+
+
+                        // pipe archive data to the file
+                        archive.pipe(output);
+
+                        // good practice to catch warnings (ie stat failures and other non-blocking errors)
+                        archive.on('warning', function (err) {
+                            if (err.code === 'ENOENT') {
+                                console.log(err)
+                            } else {
+                                // throw error
+                                throw err;
+                            }
+                        });
+
+                        // good practice to catch this error explicitly
+                        archive.on('error', function (err) {
+                            throw err;
+                        });
+
+                        const sub_folder = Math.random().toString(36).substring(2, 10)
+                        archive.file(working_file, { name: `${sub_folder}/collection.json` })
+                        for (let article of data.articles) {
+                            for (let i in article.slides) {
+                                archive.file(path.join(working_path, article.slides[i].img.src), { name: `${sub_folder}/${article.slides[i].img.src}` })
+                            }
+                        }
+
+                        archive.finalize()
+                    })
+                }
+            },
+            {
+                label: 'Qui App',
+                accelerator: `${ctrl}+Q`,
+                click() {
+                    app.quit()
+                }
+            }
+        ]
+    },
+    {
+        label: "&Edit",
+        submenu: [
+            {
+                label: "New Article",
+                accelerator: `${ctrl}+N`,
+                click() {
+                    mainWindow.webContents.send('make-article')
+                }
+            },
+            {
+                label: "Remove Article",
+                accelerator: `Alt+Backspace`,
+                click() {
+                    mainWindow.webContents.send('remove-article')
+                }
+            },
+            {
+                label: "New Slide",
+                accelerator: `${ctrl}+Shift+N`,
+                click() {
+                    mainWindow.webContents.send('make-slide')
+                }
+            }, ,
+            {
+                label: "Remove Slide",
+                accelerator: `Alt+Shift+Backspace`,
+                click() {
+                    mainWindow.webContents.send('remove-slide')
+                }
+            },
+            {
+                label: "Move Slide Down",
+                accelerator: `Alt+Down`,
+                click() {
+                    mainWindow.webContents.send('move-slide-down')
+                }
+            },
+            {
+                label: "Move Slide Up",
+                accelerator: `Alt+Up`,
+                click() {
+                    mainWindow.webContents.send('move-slide-up')
+                }
+            }
+        ]
+    },
+    {
+        label: "About",
+        role: "about"
+    }
+]
+
+// Add empty space if on Mac
+if (process.platform == 'darwin') {
+    mainMenuTemplate.unshift({})
+}
+
+// Will only send the save-request
+function sendDataRequest() {
+    mainWindow.webContents.send('file:save-request')
+}
+
+// Open a folder and create collection.json if needed (else load it)
+function openFile(default_path = working_path) {
+    var new_path = dialog.showOpenDialogSync({
+        title: "Select an folder",
+        properties: [
+            'openDirectory'
+        ],
+        defaultPath: default_path
+    })
+    if (new_path != undefined) {
+        working_path = `${new_path}`
+        working_file = `${path.join(working_path, "collection.json")}`
+        fs.readFile(working_file, { encoding: 'utf16le' }, (err, data) => {
+            if (err) {
+                let data = emptyCollectionObj();
+                writeToFile(data, (err) => {
+                    if (err)
+                        console.log(err)
+                    else
+                        mainWindow.webContents.send('file:opened', data, working_path)
+                })
+            } else {
+                mainWindow.webContents.send('file:opened', JSON.parse(data), working_path)
+            }
+        })
+    }
+}
+
+// Write the JSON data to the file as a string
+function writeToFile(data, handler = (err) => {
+    if (err) { console.log(err) }
+}) {
+    fs.writeFile(working_file, JSON.stringify(data), 'utf16le', handler)
+}
+
+function notify(title, body) {
+    new Notification({ title: title, body: body }).show()
+}
---
+++ src/main.js
@@ -0,0 +1,301 @@
+(() => {
+    const { ipcRenderer } = require('electron');
+    const path = require('path')
+    const AUTO_SAVE = false;
+
+    // Main data object
+    var mainData;
+    var working_path;
+    var currentArticle;
+    var curr_slide_list_item;
+
+    var quill = new Quill('#slide_content', {
+        modules: {
+            toolbar: "#toolbar"
+            // toolbar: [
+            //     ['bold', 'italic'],
+            //     [{ 'list': 'bullet' }]
+            // ]
+        },
+        placeholder: 'Enter the contents of the slide',
+        // scrollingContainer: "#scrolling-container",
+        theme: 'snow'
+    });
+
+
+    ipcRenderer.on('file:opened', (e, data, path) => {
+        document.querySelector('body').hidden = false;
+
+        mainData = data
+        working_path = `${path}`
+
+        updateArticlesList();
+    })
+
+    ipcRenderer.on('file:save-request', (e, data) => {
+        saveProgressToObj();
+        e.sender.send('file:save-response', mainData);
+        updateArticlesList(false);
+    })
+
+    ipcRenderer.on('export-request', (event) => {
+        saveProgressToObj()
+        event.sender.send('export-response', mainData)
+    })
+
+    ipcRenderer.on('title-response', (e, result) => {
+        currentSlide = createSlideObj();
+        currentSlide.title = result;
+        let art = { title: result, slides: [currentSlide] }
+        currentArticle = art
+
+        mainData.articles.push(art)
+
+        updateArticlesList(false);
+    })
+
+    ipcRenderer.on('make-article',      makeNewArticle)
+    ipcRenderer.on('remove-article',    removeArticle)
+    ipcRenderer.on('make-slide',        makeNewSlide)
+    ipcRenderer.on('remove-slide',      removeSlide)
+    ipcRenderer.on('move-slide-up',     moveSlideUp)
+    ipcRenderer.on('move-slide-down',   moveSlideDown)
+
+    // Title input
+    document.getElementById('slide_title').addEventListener('input', (e) => {
+        currentSlide.title = e.target.value;
+        curr_slide_list_item.innerHTML = (currentSlide.title === '') ? 'No Title' : currentSlide.title
+    })
+
+    // Title de-focused
+    if (AUTO_SAVE) {
+        document.getElementById('slide_title').addEventListener('blur', (e) => {
+            saveToFile();
+        })
+    }
+
+    // Image Selector Button
+    document.getElementById("image_selector").addEventListener('click', () => {
+        saveProgressToObj();
+        ipcRenderer.invoke('select-image').then((result) => {
+            currentSlide.img.src = result.rel
+            document.getElementById("selected_image").src = result.abs
+            updateSlide();
+            if (AUTO_SAVE) {
+                saveToFile();
+            }
+        })
+    })
+
+    // Invert Image Checkbox
+    document.getElementById('inverse-fit-checkbox').addEventListener('change', (e) => {
+        currentSlide.img.reverse_fit = e.target.checked
+    })
+
+    // Remove Slide Button
+    document.getElementById("rmv_btn").addEventListener('click', () => {
+        removeSlide()
+    })
+
+    // Add Slide Button
+    document.getElementById("nxt_btn").addEventListener('click', () => {
+        makeNewSlide()
+    })
+
+    // Add Article Button
+    document.getElementById("add_art_btn").addEventListener('click', makeNewArticle)
+
+    // Remove Article Button
+    document.getElementById("remove_art").addEventListener('click', () => {
+        removeArticle()
+    })
+
+    // Move slide up Button
+    document.getElementById('move_slide_up').addEventListener('click', () => {
+        moveSlideUp()
+    })
+
+    // Move slide Down Button
+    document.getElementById('move_slide_down').addEventListener('click', () => {
+        moveSlideDown()
+    })
+
+    function updateArticlesList(update_current = true) {
+        let list = document.getElementById("articles_list");
+        clearChildren(list)
+
+        if (mainData.articles.length > 0) {
+            if (update_current) {
+                currentArticle = mainData.articles[0];
+            }
+            for (let i = 0; i < mainData.articles.length; i++) {
+                let new_it = document.createElement('li')
+                let new_it_text = document.createTextNode(mainData.articles[i].title)
+                new_it.appendChild(new_it_text)
+                new_it.value = i;
+                new_it.addEventListener('click', (e) => {
+                    saveProgressToObj()
+
+                    clearSelected(list)
+                    e.target.classList.add('selected')
+
+                    currentArticle = mainData.articles[i];
+                    updateSlidesList();
+                })
+
+                if (mainData.articles[i] === currentArticle)
+                    new_it.classList.add('selected')
+                list.appendChild(new_it);
+            }
+
+            updateSlidesList(update_current);
+        } else {
+            makeNewArticle()
+        }
+    }
+
+    function updateSlidesList(update_current = true) {
+        let list = document.getElementById("slides_list");
+        clearChildren(list);
+
+        if (update_current) {
+            currentSlide = currentArticle.slides[0];
+        }
+        for (let i = 0; i < currentArticle.slides.length; i++) {
+            let slide = currentArticle.slides[i]
+            let new_it = document.createElement('li')
+            let new_it_text = document.createTextNode(
+                (slide.title === '') ? 'No Title' : slide.title
+            )
+            new_it.appendChild(new_it_text)
+
+            new_it.value = i + 1
+            new_it.id = `slide_item_${i}`
+            new_it.addEventListener('click', (e) => {
+                saveProgressToObj();
+
+                clearSelected(list)
+                e.target.classList.add('selected')
+                currentSlide = slide
+                curr_slide_list_item = e.target
+
+                updateSlide();
+            })
+
+            if (currentArticle.slides[i] === currentSlide){
+                curr_slide_list_item = new_it
+                new_it.classList.add('selected')
+            }
+            list.appendChild(new_it);
+        }
+
+        updateSlide();
+    }
+
+    function updateSlide() {
+        document.getElementById("selected_image").src = `${path.join(working_path, currentSlide.img.src)}`
+        document.getElementById('inverse-fit-checkbox').checked = currentSlide.img.reverse_fit
+        slide_title.value = currentSlide.title;
+        quill.setContents(currentSlide.content);
+    }
+
+    function makeNewArticle() {
+        // Need to get a popup with the new title:
+
+        ipcRenderer.send('ask-title')
+    }
+
+    function removeArticle(){
+        removeItemFromArr(mainData.articles, currentArticle)
+
+        // It already makes new Article if needed
+        updateArticlesList(true)
+    }
+
+    function makeNewSlide() {
+        saveProgressToObj()
+
+        let new_slide = createSlideObj();
+        currentArticle.slides.push(new_slide)
+        currentSlide = new_slide
+
+        updateSlidesList(false);
+    }
+
+    function removeSlide(){
+        removeItemFromArr(currentArticle.slides, currentSlide)
+        if (currentArticle.slides.length === 0) {
+            removeItemFromArr(mainData.articles, currentArticle)
+
+            // It already makes new Article if needed
+            updateArticlesList(true)
+        } else {
+            updateSlidesList(true)
+        }
+    }
+
+    function moveSlideUp(){
+        saveProgressToObj()
+
+        let old_idx = currentArticle.slides.indexOf(currentSlide)
+        if (old_idx > 0) {
+            [currentArticle.slides[old_idx - 1], currentArticle.slides[old_idx]] =
+                [currentArticle.slides[old_idx], currentArticle.slides[old_idx - 1]]
+        }
+
+        updateSlidesList(false)
+    }
+
+    function moveSlideDown() {
+        saveProgressToObj()
+
+        let old_idx = currentArticle.slides.indexOf(currentSlide)
+        if (old_idx < currentArticle.slides.length - 1) {
+            [currentArticle.slides[old_idx], currentArticle.slides[old_idx + 1]] =
+                [currentArticle.slides[old_idx + 1], currentArticle.slides[old_idx]]
+        }
+
+        updateSlidesList(false)
+    }
+
+    function saveProgressToObj() {
+        // The slide content is handled by quill
+
+        currentSlide.title = slide_title.value
+        currentSlide.content = quill.getContents()
+    }
+
+    function saveToFile() {
+        saveProgressToObj()
+        ipcRenderer.send('save-request', mainData)
+    }
+
+    function createSlideObj() {
+        return { title: "", content: {}, img: { src: "", reverse_fit: false } }
+    }
+
+    function clearChildren(el) {
+        while (el.hasChildNodes()) {
+            el.removeChild(el.firstChild);
+        }
+    }
+
+    // Since it's pass-by-ref, there's no need to return the array
+    function removeItemFromArr(arr, item) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                arr.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function clearSelected(el) {
+        for (let i = 0; i < el.children.length; i++) {
+            el.children[i].classList.remove('selected')
+        }
+    }
+
+
+})();
